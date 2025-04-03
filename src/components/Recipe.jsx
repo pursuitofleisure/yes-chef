9e17af6bcbcaad3bd1b7b188e1569cda862679e3
Diana Le <le.diana.d@gmail.com>
2025-04-02 20:35:28 -0500

    add markdown plugin for recipe results

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,5 +1,12 @@
+import Markdown from 'react-markdown';
+
 function Recipe({ recipe }) {
-  return <div>{recipe}</div>;
+  return (
+    <section aria-labelledby="heading-recommended-recipe" aria-live="polite">
+      <h2 id="heading-recommended-recipe">Recommended Recipe: </h2>
+      <Markdown>{recipe}</Markdown>
+    </section>
+  );
 }
 
 export default Recipe;
